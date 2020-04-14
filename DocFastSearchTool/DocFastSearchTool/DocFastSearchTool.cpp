bb94abd4d3cc3f91ac9c4f20e1722651b9c3e035
wutong017 <wutong_017@163.com>
2020-04-14 23:32:19 +0800

    测试数据库

--- DocFastSearchTool/DocFastSearchTool/DocFastSearchTool.cpp
+++ DocFastSearchTool/DocFastSearchTool/DocFastSearchTool.cpp
@@ -16,8 +16,42 @@ void Test_DirecttinList()
 		cout << e << endl;
 }
 
+void Test_Sqlite()
+{
+	//数据库打开
+	sqlite3* db;
+	int rc = sqlite3_open("test.db", &db);
+	if (rc != SQLITE_OK)
+		printf("Open database failed.\n");
+	else
+		printf("Open database successed.\n");
+
+	//操作数据库
+	string sql = "select * from my_tb";
+	char** result;
+	int row;
+	int col;
+	char* zErrMsg = 0;
+
+	sqlite3_get_table(db, sql.c_str(), &result, &row, &col, &zErrMsg);
+
+	for (int i = 0; i <= row; ++i)
+	{
+		for (int j = 0; j < col; ++j)
+		{
+			printf("-5%s", *(result + (i * col) + j));
+		}
+		cout << endl;
+	}
+
+	sqlite3_free_table(result);
+	//关闭数据库
+	sqlite3_close(db);
+}
+
 int main(int argc, char *argv[])
 {
-	Test_DirecttinList();
+	//Test_DirecttinList();
+	Test_Sqlite();
 	return 0;
 }
